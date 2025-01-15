47e1540feb63124fa94baa373d0597cc42c07773
ShwetaJSapar <shwetasapar2004@gmail.com>
2025-01-15 18:39:10 +0530

    Add files via upload

---
+++ LeepYear.java
@@ -0,0 +1,57 @@
+//Q:Write a program to check leap year using if else. How to check whether a given year is a leap year or not
+package ANP_D0453;
+
+//Importing Classes/Files
+import java.io.*;
+
+//Class for leap-year dealing
+public class LeepYear {
+	// Method to check leap year
+	public static void isLeapYear(int year)
+	{
+		// flag to take a non-leap year by default
+		boolean is_leap_year = false;
+
+		// If year is divisible by 4
+		if (year % 4 == 0) {
+			is_leap_year = true;
+
+			// To identify whether it is a
+			// century year or not
+			if (year % 100 == 0) {
+				// Checking if year is divisible by 400
+				// therefore century leap year
+				if (year % 400 == 0)
+					is_leap_year = true;
+				else
+					is_leap_year = false;
+			}
+		}
+
+		// We land here when corresponding if fails
+		// If year is not divisible by 4
+		else
+
+			// Flag dealing- Non leap-year
+			is_leap_year = false;
+
+		if (!is_leap_year)
+			System.out.println(year + " : Non Leap-year");
+		else
+			System.out.println(year + " : Leap-year");
+	}
+
+	// Driver Code
+	public static void main(String[] args)
+	{
+		// Calling our function by
+		// passing century year not divisible by 400
+		isLeapYear(2000);
+
+		// Calling our function by
+		// passing Non-century year
+		isLeapYear(2002);
+	}
+}
+//Output: 2000 : Leap-year
+//        2002 : Non Leap-year
